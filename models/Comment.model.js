0a92c9c1e0bba30b191cba1dde95b77794b0f9fd
John Doe <johndoe@example.com>
2022-11-04 21:18:05 +0300

    сделал комментарии бэка

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -2,7 +2,17 @@ const mongoose = require("mongoose");
 
 const CommentSchema = mongoose.Schema(
   {
-    //тут модель
+    text: {type: String,
+      required: true},
+    userId: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'User'
+    },
+    reviewToPost: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Immovables"
+    }
+    
   },
   { timestamps: true }
 );
--- routes/comments.route.js
+++ routes/comments.route.js
@@ -2,4 +2,8 @@ const { Router } = require("express");
 const { commentsController } = require("../controllers/comments.controller");
 
 const router = Router();
+router.post('/',commentsController.addComment)
+router.get('/',commentsController.getComments)
+router.delete('/:id',commentsController.deleteComment)
+
 module.exports = router;
